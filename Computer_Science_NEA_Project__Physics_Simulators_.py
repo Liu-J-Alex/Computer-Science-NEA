4b9ef8f282d8ebc1d32869ad28773283d0d2552f
Liu-J-Alex <AL1039@ga.newcastle.sch.uk>
2024-09-11 23:00:17 +0100

    Added circuit builder classes (temp)

--- Computer_Science_NEA_Project__Physics_Simulators_.py
+++ Computer_Science_NEA_Project__Physics_Simulators_.py
@@ -1,24 +1,68 @@
 import time,threading,tkinter,sqlite3,pygame
 from tkinter import *
+pygame.init()
+
+screenSizeTk="1920x1080"
+screenSizePygame=(1920,1080)
+
+
+# Circuit Builder Classes
+class electron():
+    def _init_(self,voltage,speed,direction,ElectronImage,window):
+        self.voltage= voltage
+        self.speed=speed
+        self.direction= direction
+        self.ElectronImage= pygame.draw.circle(window,(255,255,0),(1,1),4.0) # In a final solution, I would replace this with an actual asset rather than a yellow pygame circle
+
+    def display(window):
+        image= pygame.draw.circle(window,(255,255,0),(1,1),4)
+
+class battery():
+    pass
+
+class resistor():
+    pass
+
+class wire():
+    pass
+
+class voltmeter():
+    pass
+
+class ammeter():
+    pass
+#_____________________________________________________________
+
+class waveNode():
+    def _init_(self,displacement,nextNode):
+        self.displacement= displacement
+        self.nextNode= nextNode
 
 
 
+
+
+#Simulation screens
 def WaveSim():
     pass
 
 def CircuitBuilder():
+
+    pass
     
 
-
 def MainMenu():
-    global CircuitBuilder,WaveSim
-    window= Tk()
-    window.geometry("1920x1080")
-    window.title("Main Menu")
-    waveButton= Button(window,text= "Wave Simulator",background="Light Blue",command=WaveSim,foreground="Black").grid(row=1,padx=10, column=0, pady=10)
-    circuitButton=Button(window,text="Circuit Builder",background="Light Blue", command=CircuitBuilder, foreground="Black").grid(row=1,padx=10,column=1,pady=10)
-    quitButton=Button(window,text="X",background="red",command=quit,foreground="White").grid(row=0,column=5,padx=10)
+    global CircuitBuilder,WaveSim,screenSizePygame,screenSizeTk
+    windowPygame=pygame.display.set(screenSizePygame)
 
+    windowTkinter= Tk()
+    windowTkinter.geometry(screenSizeTk)
+    windowTkinter.title("Main Menu")
+    waveButton= Button(windowTkinter,text= "Wave Simulator",background="Light Blue",command=WaveSim,foreground="Black").grid(row=1,padx=10, column=0, pady=10)
+    circuitButton=Button(windowTkinter,text="Circuit Builder",background="Light Blue", command=CircuitBuilder, foreground="Black").grid(row=1,padx=10,column=1,pady=10)
+    quitButton=Button(windowTkinter,text="X",background="red",command=quit,foreground="White").grid(row=0,column=5,padx=10)
+
+    electron1=electron.display(window)
     
     window.mainloop()
 
