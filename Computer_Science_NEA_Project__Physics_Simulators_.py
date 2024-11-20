7861d5439f326fd7768d3974e8db73d3d064d4f7
Alex Liu <AL1039@ga.newcastle.sch.uk>
2024-11-20 19:44:57 +0000

    Added: Reference Line functionality (Class, List and Function) A primary wave node class (Child of wave node class)
    Changed: Rewrote the wave node class and how it is being drawn to the screen

--- Computer_Science_NEA_Project__Physics_Simulators_.py
+++ Computer_Science_NEA_Project__Physics_Simulators_.py
@@ -94,7 +94,7 @@ class bulb(compoentTemplate):
         self.brightness= 0
         self.x=x
         self.y= y
-        self.r=5
+        self.r=20
         self.rect= pygame.Rect(self.x,self.y,self.r,self.r)
         
 
@@ -126,15 +126,51 @@ class ammeter(compoentTemplate):
 #_____________________________________________________________
 
 class waveNode():
-    def __init__(self,x,y):
-        self.displacement= 0
-        self.nextNode= 1
-        self.colour= (0,255,0)
-        self.x= x
-        self.y= y
-        self.pos=(x,y)
+    #To control the frequency, experiment with 1 wave node and see if I can get it to oscillate up and down before I start making the whole sim
+    def __init__(self,x):
+        self.y= 180
+        self.x=x 
+        self.r= 20
+        self.rect= pygame.Rect(self.x,self.y,self.r,self.r)
+
+    def drawNode(self,surface):
+        pygame.draw.circle(surface,(0,0,255),(self.x,self.y),self.r)
+
+    def givepos(self,nextNode):
+        pass
+
+class primaryWaveNode(waveNode):
+    def __init__(self,amplitude,frequency):
+        super.__init__()
+        self.amplitude = amplitude
+        self.frequency= frequency 
+waveNodeList=[ waveNode(25),waveNode(45),waveNode(65),waveNode(85),waveNode(105)
+                  ,waveNode(125),waveNode(145),waveNode(165),waveNode(185),waveNode(205)
+                  ,waveNode(225),waveNode(245),waveNode(265),waveNode(285),waveNode(305)
+                  ,waveNode(325),waveNode(345),waveNode(365),waveNode(385),waveNode(405)
+                  ,waveNode(425),waveNode(445),waveNode(465),waveNode(485),waveNode(505)]#This is the list of nodes that make up the wave medium
+def resetAmp():
+    for node in waveNodeList:
+        node.y= 180
+
+   
 
 
+class referenceLine():
+    def __init__(self):
+        self.x=0 
+        self.y= 180
+        self.rect= pygame.Rect(self.x,self.y,160,10)
+
+    def drawRefLine(self,surface):
+        pygame.draw.rect(surface,(0,255,0),self.rect)
+
+    def destroy(self):
+        self.destroy
+
+refline=[]
+def createReferenceLine():
+    refline.append(referenceLine)
 
 class stopwatch():
    def __init__(self,StopwatchDisplay,minutes=0,seconds=0):
@@ -142,11 +178,6 @@ class stopwatch():
         self.seconds= seconds
         self.StopwatchDisplay= StopwatchDisplay
                                         
-
-  
-
-
-
 #Simulation screens
 def WaveSim():
     global screenSizeTk
@@ -163,11 +194,11 @@ def WaveSim():
     waveMedium= pygame.display.set_mode((160,90))
     #Tkinter Buttons
 
-    referenceLineButton= Button(windowTkinter,text="Reference Line",command= Iteration1Function,foreground="Black",).grid(row=2,column=0,sticky="w")
+    referenceLineButton= Button(windowTkinter,text="Reference Line",command= createReferenceLine,foreground="Black",).grid(row=2,column=0,sticky="w")
     rulerButton= Button(windowTkinter,text="Ruler",command= Iteration1Function,foreground="Black",).grid(row=3,column=0,sticky="w")
     normalButton=Button(windowTkinter, text="Normal",command=Iteration1Function,foreground="Black").grid(row=4,column=0,sticky="w")
     slowButton= Button(windowTkinter, text="Slow", command= Iteration1Function,foreground="Black").grid(row=5,column=0,sticky="w")
-    resetButton=Button(windowTkinter, text="Reset Wave",command= Iteration1Function,foreground="Black").grid(row=6,column=0,sticky="w")
+    resetButton=Button(windowTkinter, text="Reset Wave",command= resetAmp,foreground="Black").grid(row=6,column=0,sticky="w")
 
     amplidtudeButtonInc=Button(windowTkinter,text="Increase Amplitude",command= Iteration1Function,foreground="Black").grid(row=7,column=1,sticky="s")
     amplidtudeButtonDec= Button(windowTkinter,text="Decrease Amplitude",command= Iteration1Function,foreground="Black").grid(row=7,column=2,sticky="s")
@@ -178,12 +209,6 @@ def WaveSim():
     quitButton=Button(windowTkinter,text="X",background="red",command=quit,foreground="White").grid(row=0,column=100)
     backButton= Button(windowTkinter,text="Back",background="light blue",command=windowTkinter.destroy,foreground="black").grid(row=0,column=0,sticky="w")
 
-    waveNodeList=[ waveNode(25,180),waveNode(45,180),waveNode(65,180),waveNode(85,180),waveNode(105,180)
-                  ,waveNode(125,180),waveNode(145,180),waveNode(165,180),waveNode(185,180),waveNode(205,180)
-                  ,waveNode(225,180),waveNode(245,180),waveNode(265,180),waveNode(285,180),waveNode(305,180)
-                  ,waveNode(325,180),waveNode(345,180),waveNode(365,180),waveNode(385,180),waveNode(405,180)
-                  ,waveNode(425,180),waveNode(445,180),waveNode(465,180),waveNode(485,180),waveNode(505,180)]#This is the list of nodes that make up the wave medium
-
 
     nodeNumber=len(waveNodeList)
 
@@ -192,8 +217,10 @@ def WaveSim():
        waveMedium.fill((0, 0, 0))  
        
        for nodes in range(0,nodeNumber):
-           pygame.draw.circle(waveMedium,waveNodeList[nodes].colour,(waveNodeList[nodes].pos),10)# This loop iterates through the waveNodeList for all of the wave node objects and takes in each object's x and y attribbutes
-                                                                                               
+           nodes.drawNode# This loop iterates through the waveNodeList for all of the wave node objects and takes in each object's x and y attribbutes
+
+
+                                                                              
 
        pygame.display.update()
        # schedule the next update using recursion
@@ -283,8 +310,10 @@ def CircuitBuilder():
                 for compoentType in compList: 
                     for component in compoentType:
                         Area= component.rect
-                        if Area.collidepoint(pygame.mouse.get_pos()) and pygame.MOUSEBUTTONDOWN:#This statement is used to check if a component is being hovered over 
-                                                                                            #and if the mousebutton is being left clicked at the same time
+                        if Area.collidepoint(pygame.mouse.get_pos()) and pygame.MOUSEBUTTONDOWN:
+                            #This statement is used to check if a component is being hovered over 
+                            #and if the mousebutton is being left clicked at the same time
+
                             component.drag()  #this statment uses the drag method from my comonent template method, which sets the dragging state to be true      
                             print("Dragging")
 
@@ -293,13 +322,18 @@ def CircuitBuilder():
                             component.updatePosition(component.x,component.y)
                             #This bit of code moves the position of the square to the position of the cursor
 
-                for compoentType in compList: 
-                    for component in compoentType:
-                        Area= component.rect
-               
                         if component.isDragging and pygame.MOUSEBUTTONUP:
                             component.notDrag()
                             print("Hovering")
+
+
+                # for compoentType in compList: 
+                #     for component in compoentType:
+                #         Area= component.rect
+               
+                #         if component.isDragging and pygame.MOUSEBUTTONUP:
+                #             component.notDrag()
+                #             print("Hovering")
 
 
 
@@ -372,70 +406,3 @@ MainMenu()
 
 #These functions will hold all of my main screens, e.g the main menu and the wave simulator.
 #Menu is going to be the last function in the program since it needs to run the other 2 functions that need to be defined before the menu.
-
-# import pygame
-# import tkinter as tk
-
-# class Square:
-#     def __init__(self, x, y, size, color):
-#         self.x = x
-#         self.y = y
-#         self.size = size
-#         self.color = color
-
-#     def draw(self, surface):
-#         pygame.draw.rect(surface, self.color, (self.x, self.y, self.size, self.size))
-
-# class Game:
-#     def __init__(self):
-#         pygame.init()
-#         self.screen_width, self.screen_height = 800, 600
-#         self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
-#         pygame.display.set_caption("Square Drawer")
-
-#         self.root = tk.Tk()
-#         self.root.title("Tkinter GUI")
-#         self.root.geometry("400x100")  # Adjust the window size
-
-#         self.squares = []
-
-#     def draw_square(self, x, y):# Adds the square to the squares list
-#         size = 50
-#         color = (255, 0, 0)  # Red
-
-#         new_square = Square(x, y, size, color)
-#         self.squares.append(new_square)
-
-#     def print_squares(self): # Prints the coordinates of the squares on the pygame screen
-#         print("Array of squares:") 
-#         for square in self.squares:
-#             print(f"({square.x}, {square.y})")
-#         print(self.squares)
-
-#     def run(self):
-#         running = True
-#         while running:
-#             for event in pygame.event.get():
-#                 if event.type == pygame.QUIT:
-#                     running = False
-#                 elif event.type == pygame.MOUSEBUTTONDOWN:
-#                     if event.button == 1:  # Left mouse button
-#                         x, y = event.pos
-#                         self.draw_square(x, y) # Adds an instance of square to the squares list
-#                 elif event.type == pygame.KEYDOWN:
-#                     if event.key == pygame.K_SPACE:
-#                         self.print_squares()
-
-#             self.screen.fill((255, 255, 255))  # White background
-
-#             for square in self.squares:# Draws the squares to the pygame screen
-#                 square.draw(self.screen)
-
-#             pygame.display.update()
-
-#         pygame.quit()
-#         self.root.mainloop()
-
-# if __name__ == "__main__":
-#     game = Game()
-#     game.run()
